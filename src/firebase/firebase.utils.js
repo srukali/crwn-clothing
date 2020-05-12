31617ec595b8f44f14bd6fb13c5409175ca0b86b
srukali <katelynicole.smith@gmail.com>
2020-05-12 11:33:15 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     appId: "1:543695017920:web:0966a9af75146986569a25"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    console.log('snapshot', snapShot);
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log('Error creating user', error.message);
+      }
+    }
+
+    return userRef;
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
---
+++ src/test.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('MLMfysGHqMLDxXViYiRS').collection('cartItems').doc('afHcLOCn1HShvVdGGxFF');
+
+firestore.doc('/users/MLMfysGHqMLDxXViYiRS/cartItems/afHcLOCn1HShvVdGGxFF')
+firestore.collection('/users/MLMfysGHqMLDxXViYiRS/cartItems/')
